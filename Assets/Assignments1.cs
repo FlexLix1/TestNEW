f38081d73126a0ef02cfac6ccac625a5475c809c
FlexLix1 <furkan.cirikk@gmail.com>
2021-09-10 16:15:35 +0200

    Bugfixes

--- Assets/Assignments1.cs
+++ Assets/Assignments1.cs
@@ -17,34 +17,36 @@ public class Assignments1 : ProcessingLite.GP21
         LetterA();
         LetterN();
 
-        private void LetterF()
+        }
+
+        void LetterF()
         {
             Line(1, 1, 1, 6);
             Line(1, 6, 3, 6);
             Line(1, 4, 2.5f, 4);
         }
 
-        private void LetterU()
+         void LetterU()
         {
             Line(3, 1, 3, 3);
             Line(3, 1, 5, 1);
             Line(5, 1, 5, 3);
         }
 
-        private void LetterR()
+         void LetterR()
         {
             Line(6, 1, 6, 3);
             Line(6, 3, 7, 3);
         }
 
-        private void LetterK()
+         void LetterK()
         {
             Line(8, 1, 8, 4);
             Line(8, 2.5f, 9, 4);
             Line(8, 2.5f, 9, 1);
         }
 
-        private void LetterA()
+         void LetterA()
         {
             Line(10, 1, 10, 3);
             Line(10, 3, 11, 3);
@@ -52,7 +54,7 @@ public class Assignments1 : ProcessingLite.GP21
             Line(11, 1, 11, 3);
         }
 
-        private void LetterN()
+         void LetterN()
         {
 
             Line(12, 1, 12, 3);
@@ -60,5 +62,5 @@ public class Assignments1 : ProcessingLite.GP21
             Line(13, 1, 13, 3);
         
         }
-    }
+    
 }
