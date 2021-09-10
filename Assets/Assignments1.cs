84c739699c7c7adacaa7bbaa23f000c754d3c30d
FlexLix1 <furkan.cirikk@gmail.com>
2021-09-10 15:47:49 +0200

    Initial commit

---
+++ Assets/Assignments1.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignments1 : ProcessingLite.GP21
+
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        LetterF();
+        LetterU();
+        LetterR();
+        LetterK();
+        LetterA();
+        LetterN();
+
+        private void LetterF()
+        {
+            Line(1, 1, 1, 6);
+            Line(1, 6, 3, 6);
+            Line(1, 4, 2.5f, 4);
+        }
+
+        private void LetterU()
+        {
+            Line(3, 1, 3, 3);
+            Line(3, 1, 5, 1);
+            Line(5, 1, 5, 3);
+        }
+
+        private void LetterR()
+        {
+            Line(6, 1, 6, 3);
+            Line(6, 3, 7, 3);
+        }
+
+        private void LetterK()
+        {
+            Line(8, 1, 8, 4);
+            Line(8, 2.5f, 9, 4);
+            Line(8, 2.5f, 9, 1);
+        }
+
+        private void LetterA()
+        {
+            Line(10, 1, 10, 3);
+            Line(10, 3, 11, 3);
+            Line(10, 2, 11, 2);
+            Line(11, 1, 11, 3);
+        }
+
+        private void LetterN()
+        {
+
+            Line(12, 1, 12, 3);
+            Line(12, 3, 13, 3);
+            Line(13, 1, 13, 3);
+        
+        }
+    }
+}
